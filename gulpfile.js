6806a6554d2139f29c47ea6a6be96d731642ae59
Shawn Sandy <shawnsandy04@gmail.com>
2014-02-28 18:44:10 -0500

    added task *clean

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+
+gulp.task('default', function(){
+  // place code for your default task here
+});
+gulp.task('clean', function(){
+  // place code for your default task here
+  gulp.src('production/', {read: false})
+  .pipe(clean());
+});
