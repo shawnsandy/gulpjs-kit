16b52872037067661e8da3a4105beccdeb83b166
shawnsandy <shawnsandy04@gmail.com>
2014-03-03 15:24:42 -0500

    *fixed build folder variable error *renamed to build_dir

--- gulpfile.js
+++ gulpfile.js
@@ -8,10 +8,7 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var watch = require('gulp-watch');
 
-//folder varibles
 
-var output = 'build/',
-    scriptsPath = './src/scripts/';
 
 function getFolders(dir){
     return fs.readdirSync(dir)
@@ -20,13 +17,19 @@ function getFolders(dir){
         });
 }
 
+/*********
+ * folder varibles
+ * *********/
+var gulp_build = 'build'; //your build folder (rename to friendlier)
+
+
 gulp.task('default', ['styles','htmlpages','scripts','fonts'],function(){
     // place code for your default task here
 });
 
 gulp.task('clean', function(){
-// clean up your output dir name dist or build
-    gulp.src('build', {read: false})
+// clean up your buildFolder dir name dist or build
+    gulp.src('./'+gulp_build, {read: false})
         .pipe(clean());
 });
 
@@ -34,9 +37,9 @@ gulp.task('clean', function(){
 gulp.task('styles', function(){
     //minify and move your styles
     gulp.src('./src/css/*.css')
-        .pipe(gulp.dest(output+'/css/'));
+        .pipe(gulp.dest(gulp_build+'/css/'));
     gulp.src('./bootstrap/dist/css/*.css')
-        .pipe(gulp.dest(output+'/bootstrap/'))
+        .pipe(gulp.dest(gulp_build+'/bootstrap/'))
 
 });
 
@@ -44,19 +47,19 @@ gulp.task('styles', function(){
 gulp.task('htmlpages', function(){
     //html task
     gulp.src(['./src/*.html','./src/*.png','./src/fonts/*.*'])
-        .pipe(gulp.dest(output+'/'));//html task
+        .pipe(gulp.dest(gulp_build+'/'));//html task
     gulp.src('./src/images/*.*')
-        .pipe(gulp.dest(output+'/images/'));
+        .pipe(gulp.dest(gulp_build+'/build/images/'));
 });
 
 gulp.task('scripts',function(){
     //javascripts tasks
     gulp.src('./src/js/*.*')
         .pipe(uglify())
-        .pipe(gulp.dest(output+'/js/'));
+        .pipe(gulp.dest(gulp_build+'/js/'));
     gulp.src('./src/js/vendor/*.*')
         .pipe(uglify())
-        .pipe(gulp.dest(output+'/js/vendor/'));
+        .pipe(gulp.dest(gulp_build+'/js/vendor/'));
 });
 
 gulp.task('fonts',function(){
@@ -66,9 +69,8 @@ gulp.task('fonts',function(){
 //    copy files/dependencies froom root to the source folder
 gulp.task("srcbuild", function(){
     gulp.src('./bootstrap/dist/css/*.css')
-        .pipe(gulp.dest('./src/bootstrap/css'))
+        .pipe(gulp.dest('src/bootstrap/css'))
 });
 
 
 gulp.task('test', function(){})
-
