daa525bf70827af0990038528c3a408224d6998d
shawn sandy <shawnsandy04@gmail.com>
2014-03-02 21:30:23 -0500

    Updated tasks *javascript *html *css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
 var clean = require('gulp-clean');
+var minifycss = require('gulp-minify-css');
+var util = require('gulp-util');
+var uglify = require('gulp-uglify');
 
 gulp.task('default', function(){
   // place code for your default task here
@@ -7,7 +10,22 @@ gulp.task('default', function(){
 
 gulp.task('clean', function(){
 // clean up your output dir name dist or build
-// maybe change the dist to build or whatever you like
-  gulp.src('dist/', {read: false})
+// maybe change the die name build to something sensible -- like your app_name
+  gulp.src('build/', {read: false})
   .pipe(clean());
 });
+
+
+gulp.task('styles', function(){
+   //minify and move your styles
+});
+
+gulp.task('htmlpages', function(){
+    //html task
+});
+
+gulp.task('scripts',function(){
+    //javascripts tasks
+});
+
+
