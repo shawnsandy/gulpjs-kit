30d696e0f5fad80cad59254d7cc61e21b2d2ff6f
shawn sandy <shawnsandy04@gmail.com>
2014-03-12 21:33:05 -0400

    fixed * fixed issue with running gulp-task

--- gulpfile.js
+++ gulpfile.js
@@ -1,80 +1,46 @@
-var gulp = require('gulp');
-var path = require('path');
 var fs = require('fs');
-var clean = require('gulp-clean');
-var minifycss = require('gulp-minify-css');
-var util = require('gulp-util');
-var uglify = require('gulp-uglify');
+var path = require('path');
+var es = require('event-stream');
+var gulp = require('gulp');
+var concat = require('gulp-concat');
 var rename = require('gulp-rename');
-var watch = require('gulp-watch');
+var uglify = require('gulp-uglify');
+var clean = require('gulp-clean');
 
+var scriptsPath = './src/js/';
 
-
-function getFolders(dir){
+function getFolders(dir) {
     return fs.readdirSync(dir)
-        .filter(function(file){
+        .filter(function(file) {
             return fs.statSync(path.join(dir, file)).isDirectory();
         });
 }
 
-/*********
- * folder varibles
- * *********/
-var gulp_build = 'build'; //your build folder -- user friendly names are cool
-// src directories
-var src_scripts = 'src/sripts/',
-    src_css = 'src/css/',
-    src_files = 'src/',
-    src_images = 'src_images/';
+gulp.task('scripts', function() {
+    var folders = getFolders(scriptsPath);
 
-gulp.task('default', ['styles','htmlpages','scripts','fonts'],function(){
-    // place code for your default task here
+    var tasks = folders.map(function(folder) {
+        // concat into foldername.js
+        // write to output
+        // minify
+        // rename to folder.min.js
+        // write to output again
+        return gulp.src(path.join(scriptsPath, folder, '/*.js'))
+            .pipe(concat(folder + '.js'))
+            .pipe(gulp.dest('build/'))
+            .pipe(uglify())
+            .pipe(rename(folder + '.min.js'))
+            .pipe(gulp.dest('build/'));
+    });
+
+    return es.concat.apply(null, tasks);
 });
 
-gulp.task('clean', function(){
-// clean up your buildFolder dir name dist or build
-    gulp.src('./'+gulp_build, {read: false})
+gulp.task('cleanup', function(){
+
+    gulp.src('./build/', {read: false})
         .pipe(clean());
-});
-
-//styles
-gulp.task('styles', function(){
-    //minify and move your styles
-    gulp.src('./src/css/*.css')
-        .pipe(gulp.dest(gulp_build+'/css/'));
-    gulp.src('./bootstrap/dist/css/*.css')
-        .pipe(gulp.dest(gulp_build+'/bootstrap/'))
 
 });
 
-
-gulp.task('htmlpages', function(){
-    //html task
-    gulp.src(['./src/*.html','./src/*.png','./src/fonts/*.*'])
-        .pipe(gulp.dest(gulp_build+'/'));//html task
-    gulp.src('./src/images/*.*')
-        .pipe(gulp.dest(gulp_build+'/build/images/'));
-});
-
-gulp.task('scripts',function(){
-    //javascripts tasks
-    gulp.src('./src/js/*.*')
-        .pipe(uglify())
-        .pipe(gulp.dest(gulp_build+'/js/'));
-    gulp.src('./src/js/vendor/*.*')
-        .pipe(uglify())
-        .pipe(gulp.dest(gulp_build+'/js/vendor/'));
-});
-
-gulp.task('fonts',function(){
-
-});
-
-//    copy files/dependencies froom root to the source folder
-gulp.task("srcbuild", function(){
-    gulp.src('./bootstrap/dist/css/*.css')
-        .pipe(gulp.dest('src/bootstrap/css'))
-});
-
-
-gulp.task('test', function(){})
+gulp.task('default', function(){});
